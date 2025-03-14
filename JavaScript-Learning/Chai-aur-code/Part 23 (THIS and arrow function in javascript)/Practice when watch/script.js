9bc7563268bfda8a775a00e40ea4e090e7187633
Ataullah <masud460040@gmail.com>
2025-03-14 15:53:54 +0600

    commit 01

--- JavaScript-Learning/Chai-aur-code/Part 23 (THIS and arrow function in javascript)/Practice when watch/script.js
+++ JavaScript-Learning/Chai-aur-code/Part 23 (THIS and arrow function in javascript)/Practice when watch/script.js
@@ -2,9 +2,30 @@ const user = {
     username: 'Masud',
     price: 499,
     welcome: function () {
+        console.log(this)
         return `${this.username}, welcome to the website`
     }
 }
-console.log(user.welcome())
-user.username = 'Jubaer'
-console.log(user.welcome())
+// console.log(user.welcome())
+// user.username = 'Jubaer'
+// console.log(user.welcome())
+
+// console.log(this)
+
+// function chai() {
+//     let username = 'Masud';
+//     console.log(this.username);
+// }
+// chai()
+
+// const chai = () => {
+//     let username = 'Masud';
+//     console.log(this);
+// }
+// chai()
+
+// const square = (num) => num * num;
+// const square = (num) => (num * num);
+const square = () => ({username: 'Masud'});
+
+console.log(square())
