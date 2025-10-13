5905befee0098d18a99fc95edb3afce86062327c
Ataullah Masud <167017141+Masud460@users.noreply.github.com>
2025-10-13 19:12:48 +0600

    Update README.md

--- JavaScript-Learning/Piyush Garg/Local Storage in Javascript/Practice when watch/script.js
+++ JavaScript-Learning/Piyush Garg/Local Storage in Javascript/Practice when watch/script.js
@@ -2,6 +2,9 @@ const userInput = document.getElementById('username');
 const btn = document.getElementById('btn');
 const set_username = document.getElementById("set_username");;
 
+if (!localStorage.getItem('name')) {
+    localStorage.setItem('name', 'Masud')
+}
 
 btn.addEventListener('click', () => {
     const collectedUsername = userInput.value;
@@ -10,6 +13,6 @@ btn.addEventListener('click', () => {
 })
 
 window.addEventListener('load', () => {
-    set_username.innerText = set_username.innerText + ' ' + localStorage.getItem('name');
-    
+    const name = localStorage.getItem('name');
+    set_username.innerText = set_username.innerText + ' ' + name;
 })
